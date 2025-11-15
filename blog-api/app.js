909fe45d696c6089d5be49c5bb1e138c980260e6
elvisaghaulor <aghaulorelvis@gmail.com>
2025-11-15 15:08:39 +0100

    upgrade express and install nodemon and dotenv

--- blog-api/app.js
+++ blog-api/app.js
@@ -1,15 +1,15 @@
-const express = require('express')
+import { config } from 'dotenv';
+config(); // Load .env file
+import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('<h3>Blog API</h3>')
 })
   
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 const start = async () => {
     try {
-        // await connectDb(process.env.MONGO_URI)
         app.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
@@ -18,4 +18,4 @@ const start = async () => {
     }
 };
 
-start()
+await start()
