bd5a28b1f7544ee9787b4fbb01bc7d73cc6b08e5
elvisaghaulor <aghaulorelvis@gmail.com>
2025-12-07 22:15:25 +0100

    connect to database

--- blog-api/app.js
+++ blog-api/app.js
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
 config(); // Load .env file
 import express from 'express';
+import connectDB from './db/connect.js';
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -10,6 +12,7 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
